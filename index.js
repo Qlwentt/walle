3e3d61cac7c6aa396ff5731e6ed4958fa0ea2147
Qlwentt <quai.wentt@gmail.com>
2017-04-09 11:26:48 -0700

    walle can now describe pictures given their url

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-var Botkit = require('botkit');
+const Botkit = require('botkit');
+const request = require('request');
+const WordPOS = require('wordpos');
+const Vision = require('@google-cloud/vision');
+const vision = Vision({ projectId: require('./config').projectId});
+const token = require('./config').token
+
+const getAdj = require('./helpers/getAdj')   
+
 
 var controller = Botkit.slackbot({
   debug: false
@@ -8,14 +16,12 @@ var controller = Botkit.slackbot({
 
 // connect the bot to a stream of messages
 var walle = controller.spawn({
-  token: require('./config').token
+  token: token
 });
 
-// give the bot something to listen for.
+// Hello [World] Bot
 controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
-
   bot.reply(message,'Hello yourself.');
-
 });
 
 walle.startRTM(function(err,bot,payload){
@@ -25,7 +31,7 @@ walle.startRTM(function(err,bot,payload){
 	}
 });
 
-
+//@wall-e help
 controller.hears(['help', 'who are you'], 'direct_message,direct_mention,mention', function(bot, message) {
     var msg = `I'm WALL-E, Quai's first slack bot. Quai is building me to show Slack :slack: how much she really really wants a job there. 
 Right now I respond to some simple keywords like \`hello\`. To say hello to me type \`@wall-e hello\` in a channel or simply \`hello\` in a direct message to me.
@@ -38,6 +44,8 @@ Have fun!`
     bot.reply(message, msg);
 });
 
+// @wall-e show me a cute gif
+// spits out a random wall-e gif
 controller.hears(['show me a cute gif', 'show me another cute gif'], 'direct_message,direct_mention,mention', function(bot, message) {
     var gifs = ["http://gph.is/Z0AR5o",
      "http://data.whicdn.com/images/141318272/large.gif",
@@ -47,6 +55,36 @@ controller.hears(['show me a cute gif', 'show me another cute gif'], 'direct_mes
     bot.reply(message, gifs[Math.floor(Math.random() * gifs.length)]);
 });
 
+//@wall-e describe this pic
+controller.hears('describe this pic (.*)',['direct_message','direct_mention','mention'],function(bot,message) {
+  var fileName = message.match[1];
+  // console.log("file name: ", fileName)
+  fileName = fileName.substr(1).slice(0, -1);
+  // console.log("file name: ", fileName)  
+  
+  // fileName = "https://files.slack.com/files-pri/T2J67TBT5-F4TAK0802/blackpower_yellow.jpg?pub_secret=8533972599"
+ 
+  
+  var res = request.get(fileName, {        
+        "Authorization":"Bearer " + token
+    });
+
+  vision.detectLabels(res['uri']['href']).then((results) => {
+    const labels = results[0];
+    words=labels.join(" ");
+
+    wordpos = new WordPOS();
+    wordpos.getAdjectives(words, function(response){
+      var adj = ""
+      adj = " "+ response[0] + " "
+      if (response[0] == undefined){adj=" "}
+      bot.reply(message, "This picture contains a" + adj+ labels[0])
+      bot.reply(message, "full results:" + words)
+    });
+
+  });
+
+});
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
            ______     ______     ______   __  __     __     ______
@@ -130,110 +168,110 @@ This bot demonstrates many of the core features of Botkit:
 // //     token: process.env.token
 // // }).startRTM();
 
-controller.hears(['whatup', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+// controller.hears(['whatup', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
-});
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
+//     bot.api.reactions.add({
+//         timestamp: message.ts,
+//         channel: message.channel,
+//         name: 'robot_face',
+//     }, function(err, res) {
+//         if (err) {
+//             bot.botkit.log('Failed to add emoji reaction :(', err);
+//         }
+//     });
 
 
+//     controller.storage.users.get(message.user, function(err, user) {
+//         if (user && user.name) {
+//             bot.reply(message, 'Hello ' + user.name + '!!');
+//         } else {
+//             bot.reply(message, 'Hello.');
+//         }
+//     });
+// });
 
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
+// controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+//     var name = message.match[1];
+//     controller.storage.users.get(message.user, function(err, user) {
+//         if (!user) {
+//             user = {
+//                 id: message.user,
+//             };
+//         }
+//         user.name = name;
+//         controller.storage.users.save(user, function(err, id) {
+//             bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+//         });
+//     });
+// });
+
+// controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+//     controller.storage.users.get(message.user, function(err, user) {
+//         if (user && user.name) {
+//             bot.reply(message, 'Your name is ' + user.name);
+//         } else {
+//             bot.startConversation(message, function(err, convo) {
+//                 if (!err) {
+//                     convo.say('I do not know your name yet!');
+//                     convo.ask('What should I call you?', function(response, convo) {
+//                         convo.ask('You want me to call you `' + response.text + '`?', [
+//                             {
+//                                 pattern: 'yes',
+//                                 callback: function(response, convo) {
+//                                     // since no further messages are queued after this,
+//                                     // the conversation will end naturally with status == 'completed'
+//                                     convo.next();
+//                                 }
+//                             },
+//                             {
+//                                 pattern: 'no',
+//                                 callback: function(response, convo) {
+//                                     // stop the conversation. this will cause it to end with status == 'stopped'
+//                                     convo.stop();
+//                                 }
+//                             },
+//                             {
+//                                 default: true,
+//                                 callback: function(response, convo) {
+//                                     convo.repeat();
+//                                     convo.next();
+//                                 }
+//                             }
+//                         ]);
+
+//                         convo.next();
+
+//                     }, {'key': 'nickname'}); // store the results in a field called nickname
+
+//                     convo.on('end', function(convo) {
+//                         if (convo.status == 'completed') {
+//                             bot.reply(message, 'OK! I will update my dossier...');
+
+//                             controller.storage.users.get(message.user, function(err, user) {
+//                                 if (!user) {
+//                                     user = {
+//                                         id: message.user,
+//                                     };
+//                                 }
+//                                 user.name = convo.extractResponse('nickname');
+//                                 controller.storage.users.save(user, function(err, id) {
+//                                     bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+//                                 });
+//                             });
+
+
+
+//                         } else {
+//                             // this happens if the conversation ended prematurely for some reason
+//                             bot.reply(message, 'OK, nevermind!');
+//                         }
+//                     });
+//                 }
+//             });
+//         }
+//     });
+// });
 
 
 // controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
